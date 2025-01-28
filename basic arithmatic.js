9722ed6452354edf6fb253ced4c6df4a00775177
fitrado11 <37136988+fitrado11@users.noreply.github.com>
2025-01-28 19:25:09 +0700

    Add files via upload

---
+++ basic arithmatic.js
@@ -0,0 +1,7 @@
+// learning basic arithmetic using simple JS
+// input dog age
+var dogAge = prompt("What is your dog age ?");
+// input owner age 
+var humanAge = (((dogAge-2)*4)+21);
+// announcing the owners age 
+alert("So the human age supposed to be " + humanAge);
